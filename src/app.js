c6b1bc6202370f9f0ee13ab873c6d54f6b404184
Benson Chen <tumlivein@gmail.com>
2019-10-03 15:40:04 +0800

    Challenge

--- src/app.js
+++ src/app.js
@@ -20,18 +20,22 @@ app.get('/help', (req, res) => {
 });
 
 // 
-// Goal: Setup two new routes
+// Goal: Update routes
 // 
-// 1. Setup an about route and render a page title
-// 2. Setup a weather route and render a page title
+// 1. Setup about route to render a title with HTML
+// 2. Setup a weather route to send back JSON
+//    - Object with forecast and location strings
 // 3. Test your work by visiting both in the browser
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Show weather');
+    res.send({
+        forecast: 'It is about 50 degrees',
+        location: 'Taipei, Taiwan',
+    });
 });
 
 app.listen(3000, () => {
