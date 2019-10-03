6f22ec7f19b9cee293957a5201cff71db87c6dae
Benson Chen <tumlivein@gmail.com>
2019-10-03 15:26:38 +0800

    Challenge

--- src/app.js
+++ src/app.js
@@ -10,6 +10,20 @@ app.get('/help', (req, res) => {
     res.send('Help page');
 });
 
+// 
+// Goal: Setup two new routes
+// 
+// 1. Setup an about route and render a page title
+// 2. Setup a weather route and render a page title
+// 3. Test your work by visiting both in the browser
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
+app.get('/weather', (req, res) => {
+    res.send('Show weather');
+});
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
