b696b3b82fd4e7637db3910e787d1a1d1241af65
Benson Chen <tumlivein@gmail.com>
2019-10-04 16:25:24 +0800

    67. Deploying Node.js to Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,6 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-// 
-// Goal: Render content to paragraphs
-// 
-// 1. Select the second message p from JavaScript
-// 2. Just before fetch, render loading message and empty p
-// 3. If error, render error
-// 4. If no error, render location and forecast
-// 5. Test your work! Search for errors and for valid locations
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -22,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -67,14 +68,6 @@ app.get('/weather', (req, res) => {
     })
 });
 
-// 
-// Goal: Wire up /weather
-// 
-// 1. Require geocode/forecast into app.js
-// 2. Use the address to geocode
-// 3. Use the coordinates to get forecast
-// 4. Send back the real forecast and location
-
 app.get('/products', (req, res) => {
     if (!req.query.search) {
         return res.send({
@@ -103,6 +96,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
 });
