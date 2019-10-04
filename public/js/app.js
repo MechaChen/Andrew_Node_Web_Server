bc379894c9ec5855065a1abf13b563daafeb57ba
Benson Chen <tumlivein@gmail.com>
2019-10-04 15:12:06 +0800

    Challenge

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,5 @@
 console.log('Client side javascript file is loaded');
 
-fetch(`http://localhost:3000/weather?address=boston`).then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    });
-});
-
 const weatherForm = document.querySelector('.main-content');
 const search = document.querySelector('input');
 
@@ -20,5 +9,21 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value;
 
-    console.log(location);
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        });
+    });
 });
+
+// 
+// Goal: Use input value to get weather
+// 
+// 1. Migrate fetch call into the submit callback
+// 2. Use the search text as the address queyr string value
+// 3. Submit the form with a valid and invalid value to test
