82fb2a0631a0552e4f01c0dcdc96c392f465c08d
Benson Chen <tumlivein@gmail.com>
2019-10-04 11:54:01 +0800

    54. The Query String

--- src/app.js
+++ src/app.js
@@ -46,6 +46,18 @@ app.get('/weather', (req, res) => {
     });
 });
 
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term',
+        });
+    } 
+
+    res.send({
+        products: [],
+    });
+});
+
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
