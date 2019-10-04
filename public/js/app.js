b4c81d892af8796d63fbb42921d40f81bdad65b0
Benson Chen <tumlivein@gmail.com>
2019-10-04 15:07:17 +0800

    58. Creating a Search Form

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,6 @@
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    });
-});
+console.log('Client side javascript file is loaded');
 
-// 
-// Goal: Fetch weather! 
-// 
-// 1. Setup a call to fetch weather for Boston
-// 2. Get the parse JSON response
-//    - If error property, print error
-//    - If no error property, print location and forecast
-// 3. Refresh the browser and test your work
-
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
+fetch(`http://localhost:3000/weather?address=boston`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error);
@@ -22,4 +9,16 @@ fetch('http://localhost:3000/weather?address=boston').then((response) => {
             console.log(data.forecast);
         }
     });
+});
+
+const weatherForm = document.querySelector('.main-content');
+const search = document.querySelector('input');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    console.log(location);
 });
