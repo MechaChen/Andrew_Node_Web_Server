607b38356842eac1738cdc85003a9129bd680552
Benson Chen <tumlivein@gmail.com>
2019-10-04 17:14:44 +0800

    Challenge

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,16 @@
-const request = require('request')
+const request = require('request');
+const moment = require('moment');
+
+// 
+// Goal: Add new data to forecast
+// 
+// 1. Update the forecast string to include new data
+// 2. Commit your changes
+// 3. Push your changes to Github and deploy to Heroku
+// 4. Test your work in the live application!
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast/d4950cfcd7848f7f38a8e24385427d0e/${latitude},${longitude}`
+    const url = `https://api.darksky.net/forecast/d4950cfcd7848f7f38a8e24385427d0e/${latitude},${longitude}?units=si`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +18,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.');
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature}°C out. The highest temperature is ${body.daily.data[0].temperatureHigh}°C at ${moment(body.daily.data[0].temperatureHighTime).format('LTS')} today. There is a ${body.currently.precipProbability}% chance of rain.`);
         }
     })
 }
