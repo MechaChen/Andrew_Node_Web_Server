054b5af3f00cf27dd59a210135eab20cc8d1c5ff
Benson Chen <tumlivein@gmail.com>
2019-10-04 10:23:36 +0800

    48. Customizing the Views Directory

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
@@ -21,19 +29,11 @@ app.get('/about', (req, res) => {
     });
 });
 
-// 
-// Goal: Create a template for help page
-// 
-// 1. Setup a help template to render a help message to the screen
-// 2. Setup the help route and render the template with an example message
-// 3. Visit the rout in the browser and see your help message print
-
 app.get('/help', (req, res) => {
     res.render('help', { 
         helpText: 'This is some helpful text.',
     });
 });
-
 
 app.get('/weather', (req, res) => {
     res.send({
