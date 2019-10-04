da5334a4a05200ea3d1b8ffbd4153d3f2f4187c2
Benson Chen <tumlivein@gmail.com>
2019-10-04 10:14:27 +0800

    Challenge

--- src/app.js
+++ src/app.js
@@ -21,6 +21,20 @@ app.get('/about', (req, res) => {
     });
 });
 
+// 
+// Goal: Create a template for help page
+// 
+// 1. Setup a help template to render a help message to the screen
+// 2. Setup the help route and render the template with an example message
+// 3. Visit the rout in the browser and see your help message print
+
+app.get('/help', (req, res) => {
+    res.render('help', { 
+        helpText: 'This is some helpful text.',
+    });
+});
+
+
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is about 50 degrees',
