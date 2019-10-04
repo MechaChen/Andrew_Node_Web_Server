3968d2f2089d895965fcfe4f7ead4b2413edfb3f
Benson Chen <tumlivein@gmail.com>
2019-10-04 17:33:40 +0800

    geocode bug fixing

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -7,7 +7,7 @@ const encodeURIComponent = (address) => {
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoidHVtbGl2ZWluIiwiYSI6ImNrMWExc29yaTB4YjgzbW4ycmM3dnpyZngifQ.jaDrQvaHZAXResYN_7Th2Q&limit=1`;
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Unable to connect to location services!', undefined);
         } else if (body.features.length === 0) {
