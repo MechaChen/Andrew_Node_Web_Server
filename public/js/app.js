45382af0ea4e44f7a2dbc9826877a8478478b60a
Benson Chen <tumlivein@gmail.com>
2019-10-04 15:34:16 +0800

    Challenge

--- public/js/app.js
+++ public/js/app.js
@@ -5,28 +5,31 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-messageOne.textContent = 'From Javascript';
+// 
+// Goal: Render content to paragraphs
+// 
+// 1. Select the second message p from JavaScript
+// 2. Just before fetch, render loading message and empty p
+// 3. If error, render error
+// 4. If no error, render location and forecast
+// 5. Test your work! Search for errors and for valid locations
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = search.value;
 
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error);
+                messageOne.textContent = data.error;
             } else {
-                console.log(data.location);
-                console.log(data.forecast);
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
             }
         });
     });
 });
-
-// 
-// Goal: Use input value to get weather
-// 
-// 1. Migrate fetch call into the submit callback
-// 2. Use the search text as the address queyr string value
-// 3. Submit the form with a valid and invalid value to test
