8eea904e71e18e4c6f7cee2596c92de91c035019
Benson Chen <tumlivein@gmail.com>
2019-10-04 10:07:35 +0800

    47. Dynamics Pages with Templating

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const path = require('path');
 
-console.log(__dirname);
-console.log(__filename);
-
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
-const aboutPath = path.join(__dirname, '../public/about.html');
-const helpPath = path.join(__dirname, '../public/help.html');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
-app.use(express.static(aboutPath));
-app.use(express.static(helpPath));
 
-// 
-// Goal: Create two more HTML files
-// 
-// 1. Create a html page for about with "About" title
-// 2. Create a html page for help with "Help" title
-// 3. Remove the old route handlers for both
-// 4. Visit both in the browser to test your work
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Benson Chen',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Bencons Chen',
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
