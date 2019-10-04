3f2068bec68fb83a58507bcf8ef537ed4ff89079
Benson Chen <tumlivein@gmail.com>
2019-10-04 13:52:58 +0800

    55. Building a JSON HTTP Endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
@@ -46,20 +48,32 @@ app.get('/weather', (req, res) => {
         });
     }
 
-    res.send({
-        forecast: 'It is about 50 degrees',
-        location: 'Taipei, Taiwan',
-        address: req.query.address,
-    });
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error });
+        }
+
+        forecast(latitude, longitude, (error, forecast) => {
+            if (error) {
+                return res.send(error);
+            }
+
+            res.send({ 
+                location, 
+                forecast,
+                address: req.query.address,
+            });
+        })
+    })
 });
 
 // 
-// Goal: Update weather endpoint to accept address
+// Goal: Wire up /weather
 // 
-// 1. NO address? Send back an error message
-// 2. Address? Send back the static JSON
-//    - Add address property onto JSON which returns the provided address
-// 3. Test /weather and /weather?address=taichung
+// 1. Require geocode/forecast into app.js
+// 2. Use the address to geocode
+// 3. Use the coordinates to get forecast
+// 4. Send back the real forecast and location
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
