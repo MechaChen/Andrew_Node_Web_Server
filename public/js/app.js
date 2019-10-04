c3d6023018ca87dc65cfca466392b48d3191160a
Benson Chen <tumlivein@gmail.com>
2019-10-04 14:24:52 +0800

    57. Browser HTTP Requests with Fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
 console.log('Client side javascript file is loaded');
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    });
+});
