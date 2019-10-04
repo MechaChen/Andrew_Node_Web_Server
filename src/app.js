00d933dd6ab344f669e34791a921ca9aab797828
Benson Chen <tumlivein@gmail.com>
2019-10-04 10:38:39 +0800

    49. Advanced Templating

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
-
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -32,6 +34,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', { 
         helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Benson Chen',
     });
 });
 
